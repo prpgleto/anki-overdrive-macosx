84f9ca790b9a469b7731416d8c86c0938797acbc
Paulo Gonçalves <pauloruig@gmail.com>
2018-11-16 20:41:15 +0000

    Initial Commit

---
+++ testUIMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  testUIMac
+//
+//  Created by Paulo Gonçalves on 16/11/2018.
+//  Copyright © 2018 Paulo Gonçalves. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ testUIMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  testUIMac
+//
+//  Created by Paulo Gonçalves on 16/11/2018.
+//  Copyright © 2018 Paulo Gonçalves. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
